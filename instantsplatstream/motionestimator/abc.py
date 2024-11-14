fe3133b27b6e497bb90799903c2482c52a45ae06
yindaheng98 <yindaheng98@gmail.com>
2024-11-13 21:47:23 -0800

    FixedViewMotionEstimator

--- instantsplatstream/motionestimator/abc.py
+++ instantsplatstream/motionestimator/abc.py
@@ -1,4 +1,4 @@
-from typing import List, NamedTuple
+from typing import NamedTuple
 from abc import ABC, abstractmethod
 import torch
 from gaussian_splatting import GaussianModel
---
+++ instantsplatstream/motionestimator/fixedview.py
@@ -0,0 +1,32 @@
+from typing import List
+from abc import ABC, abstractmethod
+from instantsplatstream.dataset import CameraMeta
+from .abc import Motion, MotionEstimator
+
+
+class FixedViewCameraMetaSequence(CameraMeta):
+    image_path: List[str]
+
+
+class FixedViewMotionEstimator(MotionEstimator, metaclass=ABC):
+    def __init__(self, cameras: List[FixedViewCameraMetaSequence]):
+        super().__init__()
+        for camera in cameras:
+            assert len(camera.image_path) == len(cameras[0].image_path)
+        self.cameras = cameras
+        self.length = len(cameras[0].image_path)
+        self.iter_idx = 0
+
+    @abstractmethod
+    def estimate(self, idx: int) -> Motion:
+        raise NotImplementedError
+
+    def __iter__(self) -> 'FixedViewMotionEstimator':
+        return self
+
+    def __next__(self) -> Motion:
+        if self.iter_idx >= self.length:
+            raise StopIteration
+        motion = self.estimate(self.iter_idx)
+        self.iter_idx += 1
+        return motion
