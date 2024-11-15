75c12400e4f30ddd43d95a01e67b2a67539a4d27
yindaheng98 <yindaheng98@gmail.com>
2024-11-14 22:47:55 -0800

    update_baseframe

--- instantsplatstream/motionestimator/abc.py
+++ instantsplatstream/motionestimator/abc.py
@@ -1,3 +1,4 @@
+import copy
 from typing import NamedTuple
 from abc import ABC, abstractmethod
 import torch
@@ -8,6 +9,7 @@ from gaussian_splatting.utils import quaternion_raw_multiply
 class Motion(NamedTuple):
     rotation_quaternion: torch.Tensor
     translation_vector: torch.Tensor
+    update_baseframe: bool = True
 
 
 class MotionEstimator(metaclass=ABC):
@@ -26,26 +28,30 @@ class MotionEstimator(metaclass=ABC):
 
 class MotionCompensater:
     def __init__(self, gaussians: GaussianModel, estimator: MotionEstimator):
-        self.gaussians = gaussians
+        self.initframe = gaussians
         self.estimator = estimator
 
     def to(self, device: torch.device) -> 'MotionCompensater':
-        self.gaussians = self.gaussians.to(device)
+        self.initframe = self.initframe.to(device)
         self.estimator = self.estimator.to(device)
         return self
 
     def __iter__(self) -> 'MotionCompensater':
         self.estimator = self.estimator.__iter__()
+        self.baseframe = self.initframe
         return self
 
     def __next__(self) -> GaussianModel:
+        currframe = copy.copy(self.baseframe)
         motion = self.estimator.__next__()
         if motion.translation_vector is not None:
-            assert motion.translation_vector.shape == self.gaussians._xyz.shape
+            assert motion.translation_vector.shape == self.baseframe._xyz.shape
             with torch.no_grad():
-                self.gaussians._xyz += motion.translation_vector
+                currframe._xyz = self.baseframe._xyz + motion.translation_vector
         if motion.rotation_quaternion is not None:
-            assert motion.rotation_quaternion.shape == self.gaussians._rotation.shape
+            assert motion.rotation_quaternion.shape == self.baseframe._rotation.shape
             with torch.no_grad():
-                self.gaussians._rotation = quaternion_raw_multiply(motion.rotation_quaternion, self.gaussians._rotation)
-        return self.gaussians
+                currframe._rotation = quaternion_raw_multiply(motion.rotation_quaternion, self.baseframe._rotation)
+        if motion.update_baseframe:
+            self.baseframe = currframe
+        return currframe
