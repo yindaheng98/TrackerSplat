7f0249b09312f3c7b00fe5db6f54adcf31e2444b
yindaheng98 <yindaheng98@gmail.com>
2024-11-28 19:33:03 -0800

    install them

--- setup.py
+++ setup.py
@@ -15,14 +15,18 @@ import os
 
 packages = ['instantsplatstream'] + ["instantsplatstream." + package for package in find_packages(where="instantsplatstream")]
 packages_dot = ['dot'] + ["dot." + package for package in find_namespace_packages(where="submodules/dot/dot")]
-rasterizor_root = "submodules/diff-gaussian-rasterization"
+featurefusion_root = "submodules/featurefusion"
+motionfusion_root = "submodules/motionfusion"
 rasterizor_sources = [
     "cuda_rasterizer/rasterizer_impl.cu",
     "cuda_rasterizer/forward.cu",
     "cuda_rasterizer/backward.cu",
     "rasterize_points.cu",
     "ext.cpp"]
-
+rasterizor_packages = {
+    'instantsplatstream.utils.featurefusion.diff_gaussian_rasterization': 'submodules/featurefusion/diff_gaussian_rasterization',
+    'instantsplatstream.utils.motionfusion.diff_gaussian_rasterization': 'submodules/motionfusion/diff_gaussian_rasterization',
+}
 
 cxx_compiler_flags = []
 nvcc_compiler_flags = []
@@ -33,17 +37,22 @@ if os.name == 'nt':
 
 setup(
     name="instantsplatstream",
-    packages=packages + packages_dot + ['instantsplatstream.pixelsplatter.diff_gaussian_rasterization'],
+    packages=packages + packages_dot + list(rasterizor_packages.keys()),
     package_dir={
         'instantsplatstream': 'instantsplatstream',
         'dot': 'submodules/dot/dot',
-        'instantsplatstream.pixelsplatter.diff_gaussian_rasterization': 'submodules/diff-gaussian-rasterization/diff_gaussian_rasterization',
+        **rasterizor_packages
     },
     ext_modules=[
         CUDAExtension(
-            name="instantsplatstream.pixelsplatter.diff_gaussian_rasterization._C",
-            sources=[os.path.join(rasterizor_root, source) for source in rasterizor_sources],
-            extra_compile_args={"nvcc": nvcc_compiler_flags + ["-I" + os.path.join(os.path.abspath(rasterizor_root), "third_party/glm/")]}
+            name="instantsplatstream.utils.featurefusion.diff_gaussian_rasterization._C",
+            sources=[os.path.join(featurefusion_root, source) for source in rasterizor_sources],
+            extra_compile_args={"nvcc": nvcc_compiler_flags + ["-I" + os.path.join(os.path.abspath(featurefusion_root), "third_party/glm/")]}
+        ),
+        CUDAExtension(
+            name="instantsplatstream.utils.motionfusion.diff_gaussian_rasterization._C",
+            sources=[os.path.join(motionfusion_root, source) for source in rasterizor_sources],
+            extra_compile_args={"nvcc": nvcc_compiler_flags + ["-I" + os.path.join(os.path.abspath(motionfusion_root), "third_party/glm/")]}
         ),
     ],
     cmdclass={
