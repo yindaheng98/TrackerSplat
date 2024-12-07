b46e0cdbb12ded6ffe387351c6ed2479f2487e24
yindaheng98 <yindaheng98@gmail.com>
2024-12-07 13:35:05 -0800

    scaling transform

--- instantsplatstream/motionestimator/abc.py
+++ instantsplatstream/motionestimator/abc.py
@@ -7,8 +7,9 @@ from gaussian_splatting.utils import quaternion_raw_multiply
 
 
 class Motion(NamedTuple):
-    rotation_quaternion: torch.Tensor
-    translation_vector: torch.Tensor
+    rotation_quaternion: torch.Tensor = None
+    scaling_modifier_log: torch.Tensor = None
+    translation_vector: torch.Tensor = None
     update_baseframe: bool = True
 
 
@@ -45,17 +46,33 @@ class MotionCompensater:
         self.baseframe = self.initframe
         return self
 
+    @staticmethod
+    def transform_xyz(baseframe: GaussianModel, motion: Motion) -> torch.Tensor:
+        assert motion.translation_vector.shape == baseframe._xyz.shape
+        with torch.no_grad():
+            return baseframe._xyz + motion.translation_vector
+
+    @staticmethod
+    def transform_rotation(baseframe: GaussianModel, motion: Motion) -> torch.Tensor:
+        assert motion.rotation_quaternion.shape == baseframe._rotation.shape
+        with torch.no_grad():
+            return quaternion_raw_multiply(motion.rotation_quaternion, baseframe._rotation)
+
+    @staticmethod
+    def transform_scaling(baseframe: GaussianModel, motion: Motion) -> torch.Tensor:
+        assert motion.scaling_modifier_log.shape == baseframe._scaling.shape
+        with torch.no_grad():
+            return motion.scaling_modifier_log + baseframe._scaling
+
     def __next__(self) -> GaussianModel:
         currframe = copy.copy(self.baseframe)
         motion = self.estimator.__next__()
         if motion.translation_vector is not None:
-            assert motion.translation_vector.shape == self.baseframe._xyz.shape
-            with torch.no_grad():
-                currframe._xyz = self.baseframe._xyz + motion.translation_vector
+            currframe._xyz = self.transform_xyz(self.baseframe, motion)
         if motion.rotation_quaternion is not None:
-            assert motion.rotation_quaternion.shape == self.baseframe._rotation.shape
-            with torch.no_grad():
-                currframe._rotation = quaternion_raw_multiply(motion.rotation_quaternion, self.baseframe._rotation)
+            currframe._rotation = self.transform_rotation(self.baseframe, motion)
+        if motion.scaling_modifier_log is not None:
+            currframe._scaling = self.transform_scaling(self.baseframe, motion)
         # TODO: Training the model
         if motion.update_baseframe:
             self.baseframe = currframe
