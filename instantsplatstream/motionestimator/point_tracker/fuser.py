870e521b0c1240c1483363bad773712e7ab7118e
yindaheng98 <yindaheng98@gmail.com>
2024-12-13 21:05:21 -0800

    saperate mask & confidence

--- instantsplatstream/motionestimator/point_tracker/fuser.py
+++ instantsplatstream/motionestimator/point_tracker/fuser.py
@@ -52,30 +52,34 @@ class BaseMotionFuser(MotionFuser):
         X, Y, A = solve_transform(mean, cov3D, camera.FoVx, camera.FoVy, camera.image_width, camera.image_height, camera.world_view_transform, camera.full_proj_transform, transform2d)
         return X, Y, A, valid_mask, weights, pixhit
 
-    def compute_valid_mask_and_weights_3d(self, v11, v12, U, S, alpha, pixhits, A_count):
+    def compute_valid_mask_and_weights_3d(self, v11, v12, U, S, A_count, viewhits, alpha, pixhits):
         '''Overload this method to make your own mask and weights'''
-        valid_mask_cov, weights_cov = self.compute_valid_mask_and_weights_cov3D(v11, v12, alpha, pixhits)
-        valid_mask_mean, weights_mean = self.compute_valid_mask_and_weights_mean3D(U, S, A_count)
+        valid_mask_cov, weights_cov = self.compute_valid_mask_and_weights_cov3D(v11, v12, viewhits, alpha, pixhits)
+        valid_mask_mean, weights_mean = self.compute_valid_mask_and_weights_mean3D(U, S, A_count, viewhits, alpha, pixhits)
         valid_mask = valid_mask_cov & valid_mask_mean
         weights = weights_cov[valid_mask[valid_mask_cov]] * weights_mean[valid_mask[valid_mask_mean]]
         return valid_mask, weights
 
-    def compute_valid_mask_and_weights_cov3D(self, v11, v12, alpha, pixhits):
+    def compute_valid_mask_and_weights_cov3D(self, v11, v12, viewhits, alpha, pixhits):
         '''Overload this method to make your own mask and weights'''
+        valid_mask = (viewhits > 2) & (alpha > 1e-3) & (pixhits > 3)
         v11_scaled = v11 / alpha.unsqueeze(-1).unsqueeze(-1)
         det = torch.linalg.det(v11_scaled)
-        valid_mask = (alpha > 1e-3) & (det > 1e-12)
-        weights = alpha[valid_mask]
+        det_clamp = 1e-12
+        valid_mask &= (det > det_clamp)
+        det_log = -math.log(det_clamp)-torch.log(det[valid_mask])  # det_log\in(0, +\infty), det=1e-12->det_log=0, det=0->det_log=+\infty
+        det_weights = (torch.sigmoid(det_log) - 0.5) * 2  # det_weights\in(0, 1), det=1e-12->det_weights=0, det=0->det_weights=1
+        weights = alpha[valid_mask] * det_weights
         return valid_mask, weights
 
-    def compute_valid_mask_and_weights_mean3D(self, U, S, A_count):
+    def compute_valid_mask_and_weights_mean3D(self, U, S, A_count, viewhits, alpha, pixhits):
         '''Overload this method to make your own mask and weights'''
+        valid_mask = (viewhits > 2) & (alpha > 1e-3) & (pixhits > 3)
         S_min = S.min(-1).values
         S_clamp = 1e-3
-        valid_mask = (A_count > 3) & (S_min < S_clamp)  # S_min[valid_mask]\in(0, 1e-3)
+        valid_mask &= (S_min < S_clamp) & (A_count > 2)  # S_min[valid_mask]\in(0, 1e-3)
         S_min_log = -math.log(S_clamp)-torch.log(S_min[valid_mask])  # S_min_log\in(0, +\infty), S_min=1e-3->S_min_log=0, S_min=0->S_min_log=+\infty
         weights = torch.sigmoid(S_min_log)  # weights\in(0.5, 1), S_min=1e-3->weights=0.5, S_min=0->weights=1
-        # TODO: add A_count into weights
         return valid_mask, weights
 
     def compute_best_order(self, R, S, R_base, S_base):
@@ -100,6 +104,7 @@ class BaseMotionFuser(MotionFuser):
         gaussians = self.gaussians
         weights = torch.zeros((gaussians.get_xyz.shape[0],), device=self.device, dtype=torch.float64)
         pixhits = torch.zeros((gaussians.get_xyz.shape[0],), device=self.device, dtype=torch.int)
+        viewhits = torch.zeros((gaussians.get_xyz.shape[0],), device=self.device, dtype=torch.int)
         isvd = ISVD_Mean3D(batch_size=gaussians.get_xyz.shape[0], device=self.device, dtype=torch.float32)
         ils = ILS_RotationScale(batch_size=gaussians.get_xyz.shape[0], device=self.device)
         for camera, track in zip(tqdm(cameras, desc="Computing motion"), tracks):
@@ -108,17 +113,18 @@ class BaseMotionFuser(MotionFuser):
             ils.update(X, Y, valid_mask, weight)
             weights[valid_mask] += weight
             pixhits += pixhit
-        U, S, A_count = isvd.U, isvd.S, isvd.A_count
-        S = torch.diagonal(S, dim1=-2, dim2=-1)
+            viewhits[valid_mask] += 1
+        U, S, A_count = isvd.U, torch.diagonal(isvd.S, dim1=-2, dim2=-1), isvd.A_count
         v11, v12 = ils.v11, ils.v12
-        valid_mask, weights = self.compute_valid_mask_and_weights_3d(v11, v12, U, S, weights, pixhits, A_count)
+        valid_mask_cov, weights_cov = self.compute_valid_mask_and_weights_cov3D(v11, v12, viewhits, weights, pixhits)
+        valid_mask_mean, weights_mean = self.compute_valid_mask_and_weights_mean3D(U, S, A_count, viewhits, weights, pixhits)
 
         # solve mean3D
-        mean3D, valid_mask = isvd.solve(valid_mask)
-        translation_vector = mean3D - gaussians.get_xyz[valid_mask]
+        mean3D, valid_mask = isvd.solve(valid_mask_mean)
+        translation_vector = mean3D - gaussians.get_xyz[valid_mask_mean]
 
         # solve R and S matrix
-        R, S, valid_positive_mask = ils.solve(valid_mask)
+        R, S, valid_positive_mask = ils.solve(valid_mask_cov)
         # correct the order
         rotation_base = self.gaussians._rotation[valid_positive_mask, ...]
         scaling_base = self.gaussians._scaling[valid_positive_mask, ...]
@@ -131,10 +137,12 @@ class BaseMotionFuser(MotionFuser):
         scale_curr = self.gaussians.scaling_inverse_activation(S_best)
         rotation_transform, scaling_transform = self.compute_transformation(rotation_curr, scale_curr, rotation_base, scaling_base)
         return Motion(
-            motion_mask=valid_positive_mask,
+            motion_mask_cov=valid_positive_mask,
+            motion_mask_mean=valid_mask_mean,
             rotation_quaternion=rotation_transform,
             scaling_modifier_log=scaling_transform,
             translation_vector=translation_vector,
-            confidence=None,  # TODO: implement the confidence
+            confidence_cov=weights_cov,
+            confidence_mean=weights_mean,
             update_baseframe=False
         )
