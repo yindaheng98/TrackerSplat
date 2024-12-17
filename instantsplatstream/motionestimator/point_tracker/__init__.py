1ec48cd690b081c99e17320095171a6fc4bf0f5f
yindaheng98 <yindaheng98@gmail.com>
2024-12-16 17:16:02 -0800

    build_motion_estimator in point tracker

--- instantsplatstream/motionestimator/point_tracker/__init__.py
+++ instantsplatstream/motionestimator/point_tracker/__init__.py
@@ -2,3 +2,20 @@ from .abc import PointTrackSequence, PointTracker, MotionFuser, PointTrackMotion
 from .dot import DotPointTracker, DotMotionEstimator, Cotracker3DotMotionEstimator, TapirDotMotionEstimator, BootsTapirDotMotionEstimator
 from .cotracker import Cotracker3PointTracker, Cotracker3MotionEstimator
 from .fuser import BaseMotionFuser
+
+
+def build_motion_estimator(estimator: str, fuser: MotionFuser, device="cuda", *args, **kwargs) -> PointTrackMotionEstimator:
+    match estimator:
+        case "dot":
+            estimator = DotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
+        case "dot-tapir":
+            estimator = TapirDotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
+        case "dot-bootstapir":
+            estimator = BootsTapirDotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
+        case "dot-cotracker3":
+            estimator = Cotracker3DotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
+        case "cotracker3":
+            estimator = Cotracker3MotionEstimator(fuser=fuser, device=device, *args, **kwargs)
+        case _:
+            raise ValueError(f"Unknown extractor: {estimator}")
+    return estimator
--- motionestimation.py
+++ motionestimation.py
@@ -9,7 +9,7 @@ from gaussian_splatting import GaussianModel
 from gaussian_splatting.train import save_cfg_args
 from instantsplatstream.dataset import prepare_fixedview_dataset, VideoCameraDataset
 from instantsplatstream.motionestimator import FixedViewMotionEstimator, MotionCompensater
-from instantsplatstream.motionestimator.point_tracker import Cotracker3DotMotionEstimator, Cotracker3MotionEstimator, BaseMotionFuser
+from instantsplatstream.motionestimator.point_tracker import Cotracker3DotMotionEstimator, Cotracker3MotionEstimator, BaseMotionFuser, build_motion_estimator
 
 parser = ArgumentParser()
 parser.add_argument("--sh_degree", default=3, type=int)
@@ -40,19 +40,15 @@ def save_frame_cfg_args(sh_degree: int, source: str, destination: str, frame_fol
     return gaussians_folder
 
 
-def main(sh_degree: int, source: str, destination: str, iteration: int, device: str, args):
-    gaussians_folder = save_frame_cfg_args(sh_degree, source, destination, args.frame_folder_fmt, args.start_frame)
-    gaussians = prepare_gaussians(
-        sh_degree=sh_degree, device=device,
-        load_ply=os.path.join(gaussians_folder, "point_cloud", "iteration_" + str(args.iteration_init), "point_cloud.ply"))
-    dataset = prepare_fixedview_dataset(
-        source=source, device=device,
-        frame_folder_fmt=args.frame_folder_fmt, start_frame=args.start_frame, n_frames=None,
-        load_camera=args.load_camera)
+def build_motion_compensater(gaussians: GaussianModel, dataset: VideoCameraDataset, device, batch_size, **kwargs) -> MotionCompensater:
+    batch_func = build_motion_estimator(estimator="dot-cotracker3", fuser=BaseMotionFuser(gaussians), device=device, **kwargs)
+    motion_estimator = FixedViewMotionEstimator(dataset=dataset, batch_func=batch_func, device=device, batch_size=batch_size)
+    motion_compensater = MotionCompensater(gaussians=gaussians, estimator=motion_estimator, device=device)
+    return motion_compensater
+
+
+def main(motion_compensater: MotionCompensater, dataset: VideoCameraDataset, gaussians_folder, sh_degree: int, source: str, destination: str, iteration: int, args):
     dataset[0].save_cameras(os.path.join(gaussians_folder, "cameras.json"))
-    batch_func = Cotracker3DotMotionEstimator(fuser=BaseMotionFuser(gaussians), device=device, rescale_factor=args.tracking_rescale)
-    motion_estimator = FixedViewMotionEstimator(dataset, batch_func, batch_size=args.batch_size, device=device)
-    motion_compensater = MotionCompensater(gaussians, motion_estimator, device=device)
     for i, frame_gaussians in enumerate(motion_compensater):
         gaussians_folder = save_frame_cfg_args(sh_degree, source, destination, args.frame_folder_fmt, args.start_frame + i + 1)
         save_path = os.path.join(gaussians_folder, "point_cloud", "iteration_" + str(iteration))
@@ -65,5 +61,14 @@ def main(sh_degree: int, source: str, destination: str, iteration: int, device: 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    gaussians_folder = save_frame_cfg_args(args.sh_degree, args.source, args.destination, args.frame_folder_fmt, args.start_frame)
     with torch.no_grad():
-        main(args.sh_degree, args.source, args.destination, args.iteration, args.device, args)
+        gaussians = prepare_gaussians(
+            sh_degree=args.sh_degree, device=args.device,
+            load_ply=os.path.join(gaussians_folder, "point_cloud", "iteration_" + str(args.iteration_init), "point_cloud.ply"))
+        dataset = prepare_fixedview_dataset(
+            source=args.source, device=args.device,
+            frame_folder_fmt=args.frame_folder_fmt, start_frame=args.start_frame, n_frames=None,
+            load_camera=args.load_camera)
+        motion_compensater = build_motion_compensater(gaussians, dataset, args.device, args.batch_size, rescale_factor=args.tracking_rescale)
+        main(motion_compensater, dataset, gaussians_folder, args.sh_degree, args.source, args.destination, args.iteration, args)
