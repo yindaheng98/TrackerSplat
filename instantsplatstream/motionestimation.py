47ba2ec3c52495f2f1fc619e03368fc502ecbab8
yindaheng98 <yindaheng98@gmail.com>
2024-12-16 18:02:48 -0800

    run as modules

--- instantsplatstream/motionestimation.py
+++ instantsplatstream/motionestimation.py
@@ -3,26 +3,11 @@ import os
 from tqdm import tqdm
 from os import makedirs
 from functools import partial
-from argparse import ArgumentParser
 from gaussian_splatting import GaussianModel
 import gaussian_splatting.train
 from instantsplatstream.dataset import prepare_fixedview_dataset, VideoCameraDataset
 from instantsplatstream.motionestimator import FixedViewMotionEstimator, MotionCompensater
 from instantsplatstream.motionestimator.point_tracker import BaseMotionFuser, build_motion_estimator
-
-parser = ArgumentParser()
-parser.add_argument("--sh_degree", default=3, type=int)
-parser.add_argument("-s", "--source", required=True, type=str)
-parser.add_argument("-d", "--destination", required=True, type=str)
-parser.add_argument("-i", "--iteration", required=True, type=int)
-parser.add_argument("--iteration_init", required=True, type=str, help="iteration of the initial gaussians")
-parser.add_argument("-f", "--frame_folder_fmt", default="frame%d", type=str, help="frame folder format string")
-parser.add_argument("-n", "--n_frames", default=None, type=int, help="number of frames to process")
-parser.add_argument("-b", "--batch_size", default=3, type=int, help="batch size of point tracking")
-parser.add_argument("--load_camera", default=None, type=str)
-parser.add_argument("--device", default="cuda", type=str)
-parser.add_argument("--start_frame", default=1, type=int, help="start from which frame")
-parser.add_argument("--tracking_rescale", default=1.0, type=float)
 
 
 def prepare_gaussians(sh_degree: int, device: str, load_ply: str) -> GaussianModel:
@@ -39,8 +24,8 @@ def save_cfg_args(sh_degree: int, source: str, destination: str, frame_folder_fm
     return destination_folder
 
 
-def build_motion_compensater(gaussians: GaussianModel, dataset: VideoCameraDataset, device: torch.device, batch_size: int, **kwargs) -> MotionCompensater:
-    batch_func = build_motion_estimator(estimator="dot-cotracker3", fuser=BaseMotionFuser(gaussians), device=device, **kwargs)
+def build_motion_compensater(estimator: str, gaussians: GaussianModel, dataset: VideoCameraDataset, device: torch.device, batch_size: int, **kwargs) -> MotionCompensater:
+    batch_func = build_motion_estimator(estimator=estimator, fuser=BaseMotionFuser(gaussians), device=device, **kwargs)
     motion_estimator = FixedViewMotionEstimator(dataset=dataset, batch_func=batch_func, device=device, batch_size=batch_size)
     motion_compensater = MotionCompensater(gaussians=gaussians, estimator=motion_estimator, device=device)
     return motion_compensater
@@ -58,17 +43,32 @@ def motion_compensate(motion_compensater: MotionCompensater, dataset: VideoCamer
 
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--sh_degree", default=3, type=int)
+    parser.add_argument("-s", "--source", required=True, type=str)
+    parser.add_argument("-d", "--destination", required=True, type=str)
+    parser.add_argument("-i", "--iteration", required=True, type=int)
+    parser.add_argument("--load_camera", default=None, type=str)
+    parser.add_argument("--device", default="cuda", type=str)
+
+    parser.add_argument("--estimator", choices=["dot", "dot-tapir", "dot-bootstapir", "dot-cotracker3", "cotracker3"], default="dot-cotracker3")
+    parser.add_argument("--iteration_init", required=True, type=str, help="iteration of the initial gaussians")
+    parser.add_argument("-f", "--frame_folder_fmt", default="frame%d", type=str, help="frame folder format string")
+    parser.add_argument("-n", "--n_frames", default=None, type=int, help="number of frames to process")
+    parser.add_argument("-b", "--batch_size", default=3, type=int, help="batch size of point tracking")
+    parser.add_argument("--start_frame", default=1, type=int, help="start from which frame")
+    parser.add_argument("--tracking_rescale", default=1.0, type=float)
     args = parser.parse_args()
     save_frame_cfg_args = partial(save_cfg_args, sh_degree=args.sh_degree, source=args.source, destination=args.destination, frame_folder_fmt=args.frame_folder_fmt)
-    destination_folder = save_frame_cfg_args(frame=args.start_frame)
+    load_ply = os.path.join(args.destination, args.frame_folder_fmt % args.start_frame, "point_cloud", "iteration_" + str(args.iteration_init), "point_cloud.ply")
     with torch.no_grad():
         gaussians = prepare_gaussians(
             sh_degree=args.sh_degree, device=args.device,
-            load_ply=os.path.join(destination_folder, "point_cloud", "iteration_" + str(args.iteration_init), "point_cloud.ply"))
+            load_ply=load_ply)
         dataset = prepare_fixedview_dataset(
             source=args.source, device=args.device,
             frame_folder_fmt=args.frame_folder_fmt, start_frame=args.start_frame, n_frames=None,
             load_camera=args.load_camera)
-        dataset[0].save_cameras(os.path.join(destination_folder, "cameras.json"))
-        motion_compensater = build_motion_compensater(gaussians, dataset, args.device, args.batch_size, rescale_factor=args.tracking_rescale)
+        motion_compensater = build_motion_compensater(args.estimator, gaussians, dataset, args.device, args.batch_size, rescale_factor=args.tracking_rescale)
         motion_compensate(motion_compensater, dataset, save_frame_cfg_args, args.iteration, args.start_frame, args.n_frames)
