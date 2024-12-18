4f71a8833732855c70717e3bd0b4ab822263b327
yindaheng98 <yindaheng98@gmail.com>
2024-12-17 21:26:53 -0800

    put into visualizer

--- instantsplatstream/motionestimator/point_tracker/__init__.py
+++ instantsplatstream/motionestimator/point_tracker/__init__.py
@@ -5,17 +5,10 @@ from .fuser import BaseMotionFuser
 
 
 def build_motion_estimator(estimator: str, fuser: MotionFuser, device="cuda", *args, **kwargs) -> PointTrackMotionEstimator:
-    match estimator:
-        case "dot":
-            estimator = DotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
-        case "dot-tapir":
-            estimator = TapirDotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
-        case "dot-bootstapir":
-            estimator = BootsTapirDotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
-        case "dot-cotracker3":
-            estimator = Cotracker3DotMotionEstimator(fuser=fuser, device=device, *args, **kwargs)
-        case "cotracker3":
-            estimator = Cotracker3MotionEstimator(fuser=fuser, device=device, *args, **kwargs)
-        case _:
-            raise ValueError(f"Unknown extractor: {estimator}")
-    return estimator
+    return {
+        "dot": DotMotionEstimator,
+        "dot-tapir": TapirDotMotionEstimator,
+        "dot-bootstapir": BootsTapirDotMotionEstimator,
+        "dot-cotracker3": Cotracker3DotMotionEstimator,
+        "cotracker3": Cotracker3MotionEstimator,
+    }[estimator](fuser=fuser, device=device, *args, **kwargs)
