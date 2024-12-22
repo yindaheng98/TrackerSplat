39f12c5b6dbb13894b1cd372a983a84d939eff7b
yindaheng98 <yindaheng98@gmail.com>
2024-12-21 21:58:33 -0800

    postcompute_valid_mask_and_weights

--- instantsplatstream/motionestimator/point_tracker/fuser.py
+++ instantsplatstream/motionestimator/point_tracker/fuser.py
@@ -78,7 +78,7 @@ class BaseMotionFuser(MotionFuser):
         valid_mask = (hits_in_view_mask & (fixed_avg > avg_in_view_threshold)).any(dim=-1)
         return valid_mask, fixed_avg.sum(-1)[valid_mask] * viewhits[valid_mask] / viewhits.max()
 
-    def compute_valid_mask_and_weights_cov3D(self, v11, v12, viewhits, alpha, pixhits):
+    def precompute_valid_mask_and_weights_cov3D(self, v11, v12, viewhits, alpha, pixhits):
         '''Overload this method to make your own mask and weights'''
         valid_mask = (viewhits > 2) & (alpha > 1e-3) & (pixhits > 3)
         v11_scaled = v11[valid_mask, ...] / alpha[valid_mask, ...].unsqueeze(-1).unsqueeze(-1)
@@ -91,7 +91,17 @@ class BaseMotionFuser(MotionFuser):
         weights = alpha[valid_mask] * det_weights
         return valid_mask, weights
 
-    def compute_valid_mask_and_weights_mean3D(self, U, S, A_count, viewhits, alpha, pixhits):
+    def postcompute_valid_mask_and_weights_cov3D(self, R, S, error, weight, mask, viewhits, alpha, pixhits):
+        '''Overload this method to make your own mask and weights'''
+        # return R, S, mask, weight
+        error_avg = error.abs().sum(-1)/alpha[mask]
+        error_clamp = 0.5
+        small_mask = error_avg < error_clamp
+        mask = mask.clone()
+        mask[mask.clone()] = small_mask
+        return R[small_mask], S[small_mask], mask, weight[small_mask] * (1 - error_avg[small_mask])
+
+    def precompute_valid_mask_and_weights_mean3D(self, U, S, A_count, viewhits, alpha, pixhits):
         '''Overload this method to make your own mask and weights'''
         valid_mask = (viewhits > 2) & (alpha > 1e-3) & (pixhits > 3)
         S_min = S.min(-1).values
@@ -100,6 +110,16 @@ class BaseMotionFuser(MotionFuser):
         S_min_log = -math.log(S_clamp)-torch.log(S_min[valid_mask])  # S_min_log\in(0, +\infty), S_min=1e-3->S_min_log=0, S_min=0->S_min_log=+\infty
         weights = torch.sigmoid(S_min_log)  # weights\in(0.5, 1), S_min=1e-3->weights=0.5, S_min=0->weights=1
         return valid_mask, weights
+
+    def postcompute_valid_mask_and_weights_mean3D(self, mean3D, error, weight, mask, viewhits, alpha, pixhits):
+        '''Overload this method to make your own mask and weights'''
+        # return mean3D, mask, weight
+        error_avg = error.abs().sum(-1)/alpha[mask]
+        error_clamp = 0.5
+        small_mask = error_avg < error_clamp
+        mask = mask.clone()
+        mask[mask.clone()] = small_mask
+        return mean3D[small_mask], mask, weight[small_mask] * (1 - error_avg[small_mask])
 
     def compute_best_order(self, R, S, R_base, S_base):
         '''Overload this method to make your own order'''
@@ -141,31 +161,34 @@ class BaseMotionFuser(MotionFuser):
             fixed_sums[fixed_idx, i] = fixed_sum
             fixed_alphas[fixed_idx, i] = fixed_alpha
             fixed_pixhits[:, i] = pixhit
+
+        # solve cov and mean mask
         U, S, A_count = isvd.U, torch.diagonal(isvd.S, dim1=-2, dim2=-1), isvd.A_count
         v11, v12 = ils.v11, ils.v12
-        valid_mask_cov, weights_cov = self.compute_valid_mask_and_weights_cov3D(v11, v12, viewhits, weights, pixhits)
-        valid_mask_mean, weights_mean = self.compute_valid_mask_and_weights_mean3D(U, S, A_count, viewhits, weights, pixhits)
+        valid_mask_cov, weights_cov = self.precompute_valid_mask_and_weights_cov3D(v11, v12, viewhits, weights, pixhits)
+        valid_mask_mean, weights_mean = self.precompute_valid_mask_and_weights_mean3D(U, S, A_count, viewhits, weights, pixhits)
+
+        # solve fixed mask
         fixed_mask, weights_fixed = self.compute_fixed_mask_and_weights(fixed_sums, fixed_alphas, fixed_pixhits, viewhits, weights, pixhits)
+        weights_cov = weights_cov[(~fixed_mask)[valid_mask_cov]]
+        valid_mask_cov &= ~fixed_mask
+        weights_mean = weights_mean[(~fixed_mask)[valid_mask_mean]]
+        valid_mask_mean &= ~fixed_mask
 
         # solve mean3D
-        mean3D, mean3Derror, valid_mask_mean_solved = isvd.solve(valid_mask_mean & (~fixed_mask))  # ! Solve mean3D may be time consuming
-        # re select them
-        mean3D = mean3D[(valid_mask_mean & (~fixed_mask))[valid_mask_mean_solved]]
-        # re select weights
-        weights_mean = weights_mean[(valid_mask_mean_solved & (~fixed_mask))[valid_mask_mean]]
-        valid_mask_mean &= valid_mask_mean_solved & (~fixed_mask)
+        mean3D, mean3Derror, valid_mask_mean_solved = isvd.solve(valid_mask_mean)  # ! Solve mean3D may be time consuming
+        # select mean3D
+        weights_mean = weights_mean[valid_mask_mean_solved[valid_mask_mean]]
+        mean3D, valid_mask_mean, weights_mean = self.postcompute_valid_mask_and_weights_mean3D(mean3D, mean3Derror, weights_mean, valid_mask_mean_solved, viewhits, weights, pixhits)
 
         # mean3D motion vector
         translation_vector = mean3D - gaussians.get_xyz[valid_mask_mean]
 
         # solve R and S matrix
-        R, S, coverror, valid_mask_cov_solved = ils.solve(valid_mask_cov & (~fixed_mask))  # ! Solve R,S may consume a lot of memory
-        # re select them
-        R = R[(valid_mask_cov & (~fixed_mask))[valid_mask_cov_solved]]
-        S = S[(valid_mask_cov & (~fixed_mask))[valid_mask_cov_solved]]
-        # re select weights
-        weights_cov = weights_cov[(valid_mask_cov_solved & (~fixed_mask))[valid_mask_cov]]
-        valid_mask_cov &= valid_mask_cov_solved & (~fixed_mask)
+        R, S, coverror, valid_mask_cov_solved = ils.solve(valid_mask_cov)  # ! Solve R,S may consume a lot of memory
+        # select mean3D
+        weights_cov = weights_cov[valid_mask_cov_solved[valid_mask_cov]]
+        R, S, valid_mask_cov, weights_cov = self.postcompute_valid_mask_and_weights_cov3D(R, S, coverror, weights_cov, valid_mask_cov_solved, viewhits, weights, pixhits)
 
         # correct the order
         rotation_base = self.gaussians._rotation[valid_mask_cov, ...]
