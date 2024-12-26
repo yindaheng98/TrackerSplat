baf8a92ef582639e9f856db63e553692a3019c7a
yindaheng98 <yindaheng98@gmail.com>
2024-12-25 22:38:03 -0800

    propogate neighbor_fix

--- instantsplatstream/motionestimator/compensater/regularization.py
+++ instantsplatstream/motionestimator/compensater/regularization.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 from gaussian_splatting import GaussianModel
 from instantsplatstream.motionestimator import Motion
-from instantsplatstream.utils import axis_angle_to_quaternion, quaternion_to_axis_angle, propagate, motion_median_filter
+from instantsplatstream.utils import axis_angle_to_quaternion, quaternion_to_axis_angle, propagate
 
 from .base import transform_xyz, transform_rotation, transform_scaling
 from .filter import FilteredMotionCompensater
@@ -35,19 +35,32 @@ class PropagatedMotionCompensater(FilteredMotionCompensater):
         # prop_translation_vector *= (prop_confidence / (prop_confidence + fix_confidence)).unsqueeze(-1) # TODO: fix 0 division
         return prop_translation_vector
 
+    def compute_neighbor_fix(self, fixed_mask: torch.Tensor, confidence_fix: torch.Tensor, n_iter=100) -> torch.Tensor:
+        '''If 3/4 neighbors are fixed, the point is fixed'''
+        fixed_mask = fixed_mask.clone()
+        for _ in range(n_iter):
+            new_fixed_mask = fixed_mask[self.neighbor_indices[~fixed_mask]].sum(-1) >= self.k * 3 // 4
+            fixed_mask[~fixed_mask] = new_fixed_mask
+            if new_fixed_mask.sum() <= 0:
+                break
+        return fixed_mask
+
     def compensate(self, baseframe: GaussianModel, motion: Motion) -> GaussianModel:
         '''Overload this method to make your own compensation'''
         currframe = copy.deepcopy(baseframe)
         median_translation_vector = self.median_filter_neighbor_transformation(motion.translation_vector, motion.motion_mask_mean)
-        rotation = self.compute_neighbor_rotation(motion.rotation_quaternion, motion.motion_mask_cov, motion.confidence_cov, motion.fixed_mask)
-        translation = self.compute_neighbor_transformation(median_translation_vector, motion.motion_mask_mean, motion.confidence_mean, motion.fixed_mask)
-        rotation[motion.fixed_mask, 0] = 1
-        rotation[motion.fixed_mask, 1:] = 0
-        translation[motion.fixed_mask, :] = 0
+        fixed_mask = self.compute_neighbor_fix(motion.fixed_mask, motion.confidence_fix)
+        rotation = self.compute_neighbor_rotation(motion.rotation_quaternion, motion.motion_mask_cov, motion.confidence_cov, fixed_mask)
+        translation = self.compute_neighbor_transformation(median_translation_vector, motion.motion_mask_mean, motion.confidence_mean, fixed_mask)
+        rotation[fixed_mask, 0] = 1
+        rotation[fixed_mask, 1:] = 0
+        translation[fixed_mask, :] = 0
         currframe._xyz = nn.Parameter(transform_xyz(baseframe, translation))
         currframe._rotation = nn.Parameter(transform_rotation(baseframe, rotation))
         if motion.scaling_modifier_log is not None:
             scaling_modifier_log = motion.scaling_modifier_log
-            scaling_modifier_log[motion.fixed_mask[motion.motion_mask_cov], :] = 0
+            scaling_modifier_log[fixed_mask[motion.motion_mask_cov], :] = 0
             currframe._scaling = nn.Parameter(transform_scaling(baseframe, scaling_modifier_log, motion.motion_mask_cov))
+        # with torch.no_grad():
+        #     currframe._opacity[fixed_mask] += currframe.inverse_opacity_activation(torch.tensor(0.05, device=currframe._opacity.device))  # debug
         return currframe
