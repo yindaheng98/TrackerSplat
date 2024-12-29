5be4090a75b933a41c820679ae35cfb8e8c92fe8
yindaheng98 <yindaheng98@gmail.com>
2024-12-28 20:55:23 -0800

    draw motion

--- instantsplatstream/incrementalrender.py
+++ instantsplatstream/incrementalrender.py
@@ -10,6 +10,8 @@ from gaussian_splatting.utils import psnr
 from gaussian_splatting.utils.lpipsPyTorch import lpips
 from gaussian_splatting.render import prepare_rendering
 from instantsplatstream.utils.motionfusion import compute_mean2D
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def prepare_base(sh_degree: int, device: str, mode: str, load_ply: str) -> Tuple[CameraDataset, GaussianModel]:
@@ -25,8 +27,25 @@ def prepare_base(sh_degree: int, device: str, mode: str, load_ply: str) -> Tuple
     return gaussians
 
 
-def draw_motion(rendering, point_image, point_image_after, save_path):
-    pass
+def draw_motion(rendering, point_image, point_image_after, save_path, threshold=16):
+    mask = ((point_image_after - point_image).abs() > threshold).any(-1)
+    rendering = (rendering * 255).permute(1, 2, 0).cpu().numpy().astype(np.uint8)
+    point_image_motion = (point_image_after[mask] - point_image[mask]).cpu().numpy()
+    point_image = point_image[mask].cpu().numpy().astype(np.int32)
+    fig = plt.figure(figsize=(rendering.shape[1], rendering.shape[0]), dpi=2)
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
+    ax.imshow(rendering)
+    ax.quiver(
+        point_image[:, 0], point_image[:, 1], point_image_motion[:, 0], point_image_motion[:, 1],
+        angles='xy', scale_units='xy', scale=1, width=0.0001, headwidth=1, minshaft=1, minlength=1,
+        color='r', alpha=0.25
+    )
+    ax.set_xlim(0, rendering.shape[1])
+    ax.set_ylim(rendering.shape[0], 0)
+    ax.axis('off')
+    fig.savefig(save_path)
+    plt.close(fig)
 
 
 def rendering(dataset: CameraDataset, gaussians: GaussianModel, gaussians_base: GaussianModel, save: str):
@@ -45,7 +64,7 @@ def rendering(dataset: CameraDataset, gaussians: GaussianModel, gaussians_base: 
         point_image_base = compute_mean2D(camera.full_proj_transform, camera.image_width, camera.image_height, gaussians_base.get_xyz.detach())
         point_image = compute_mean2D(camera.full_proj_transform, camera.image_width, camera.image_height, gaussians.get_xyz.detach())
         valid_mask = (out['radii'] > 0) & (0 < point_image).all(-1) & (point_image[:, 0] < camera.image_width) & (point_image[:, 1] < camera.image_height)
-        draw_motion(rendering, point_image[valid_mask], point_image_base[valid_mask], os.path.join(render_path, '{0:05d}'.format(idx) + "_motion.png"))
+        draw_motion(rendering, point_image_base[valid_mask], point_image[valid_mask], os.path.join(render_path, '{0:05d}'.format(idx) + "_motion.png"))
 
 
 if __name__ == "__main__":
