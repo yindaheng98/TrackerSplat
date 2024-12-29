9212d8e57fa506ebe5539aaa5a65fc85601ece12
yindaheng98 <yindaheng98@gmail.com>
2024-12-28 22:09:43 -0800

    sparse-init

---
+++ tools/sparse-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+initialize() {
+    # echo \
+    python -m instantsplat.initialize \
+        -d data/$1/frame1 \
+        --initializer colmap-sparse \
+        -o "colmap_executable='./data/colmap/COLMAP.bat'"
+    for i in $(seq 2 $2); do
+        # echo \
+        python -m instantsplat.initialize \
+            -d data/$1/frame$i \
+            --initializer colmap-sparse \
+            -o "colmap_executable='./data/colmap/COLMAP.bat'" \
+            -o "load_camera='./data/$1/frame1'"
+    done
+}
+# initialize "stepin" 300
+initialize "walking" 75
