d3bf635633999c246745111ee333978816301fc1
yindaheng98 <yindaheng98@gmail.com>
2025-04-01 23:41:45 -0700

    bug fixed

--- tools/n3dv_extract.sh
+++ tools/n3dv_extract.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-mkdir -p data/save_frame1
-wget -O data/save_frame1/save_frame1.zip https://github.com/yindaheng98/InstantSplatStream/releases/download/v0.0-camera/save_frame1.zip
-cd data/save_frame1
-unzip -o save_frame1.zip
+mkdir -p data/saved_frame1
+wget -O data/saved_frame1/saved_frame1.zip https://github.com/yindaheng98/InstantSplatStream/releases/download/v0.0-camera/saved_frame1.zip
+cd data/saved_frame1
+unzip -o saved_frame1.zip
 cd ../../
 
 convert_n3dv() {
@@ -15,7 +15,7 @@ convert_n3dv() {
         --n_frames $2 >./temp.sh &&
         chmod +x ./temp.sh && ./temp.sh && rm ./temp.sh
     # extract first camera from dataset
-    rm -rf "data/$1/frame1" && cp -r "data/save_frame1/$1/frame1" "data/$1/frame1"
+    rm -rf "data/$1/frame1" && cp -r "data/saved_frame1/$1/frame1" "data/$1/frame1"
 }
 
 convert_n3dv coffee_martini 300 "cam[0-9][0-9].mp4"
@@ -38,7 +38,7 @@ convert_stnerf() {
         fi
     done
     # extract first camera from dataset
-    rm -rf "data/$1/frame1" && cp -r "data/save_frame1/$1/frame1" "data/$1/frame1"
+    rm -rf "data/$1/frame1" && cp -r "data/saved_frame1/$1/frame1" "data/$1/frame1"
 }
 
 convert_stnerf boxing 71
