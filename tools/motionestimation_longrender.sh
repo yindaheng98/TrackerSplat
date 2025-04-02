89770debca7038796f5e20351bcbadd080986da3
yindaheng98 <yindaheng98@gmail.com>
2025-04-01 21:15:30 -0700

    render long video

---
+++ tools/motionestimation_longrender.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+FFMPEG="ffmpeg"
+render() {
+    python -m cagstream.render \
+        -s output/$1/$2 \
+        -d output/$1/$2/render \
+        --source_init output/$1/frame1 \
+        --iteration_init $3 -i $4 \
+        --frame_start $5 --frame_end $6 \
+        --load_camera output/$1/frame1/cameras.json \
+        --mode base --load_camera_mode interp-json \
+        --fix_intrinsic $INTRINSIC
+    $FFMPEG -y -f image2 -i output/$1/$2/render/%05d.png -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -pix_fmt yuv420p -crf 10 output/$1/$2.mp4
+    rm -rf output/$1/$2/render
+}
+render basketball train/regularized 10000 1000 2 75
