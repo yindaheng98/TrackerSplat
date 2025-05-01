a2cba84d7e95fe0f07299f6e002d22964416320a
yindaheng98 <yindaheng98@gmail.com>
2025-04-30 22:33:03 -0700

    -b9 is train 8 in parallel

--- tools/motionestimation_long.sh
+++ tools/motionestimation_long.sh
@@ -28,7 +28,7 @@ train() {
         -b $6 -n $7 $REFININGARGS \
         --load_camera $8
 }
-# train "walking" 1 1000 track/propagate-dot-cotracker3 "" 8 100 "output/walking/frame1/cameras.json" # debug
+# train "walking" 1 1000 track/propagate-dot-cotracker3 "" 9 100 "output/walking/frame1/cameras.json" # debug
 initialize_and_train_video_allmethods() {
     CAMERAS="output/$1/frame$2/cameras.json"
     train $1 $2 $3 refine/base-propagate-dot-cotracker3 "-o rescale_factor=$4" $5 $6 "$CAMERAS"
@@ -38,25 +38,25 @@ initialize_and_train_video_allmethods() {
     train $1 $2 $3 train/hexplane "" $5 $6 "$CAMERAS"
     train $1 $2 $3 train/regularizedhexplane "-o neighbors=20" $5 $6 "$CAMERAS"
 }
-initialize_and_train_video_allmethods walking 1 1000 0.3 8 75 # debug
-initialize_and_train_video_allmethods taekwondo 1 1000 0.3 8 101
-initialize_and_train_video_allmethods boxing 1 1000 0.3 8 71
+initialize_and_train_video_allmethods walking 1 1000 0.3 9 75 # debug
+initialize_and_train_video_allmethods taekwondo 1 1000 0.3 9 101
+initialize_and_train_video_allmethods boxing 1 1000 0.3 9 71
 
-initialize_and_train_video_allmethods coffee_martini 1 1000 0.3 8 300
-initialize_and_train_video_allmethods cook_spinach 1 1000 0.3 8 300
-initialize_and_train_video_allmethods cut_roasted_beef 1 1000 0.3 8 300
-initialize_and_train_video_allmethods flame_salmon_1 1 1000 0.3 8 1200
-initialize_and_train_video_allmethods flame_steak 1 1000 0.3 8 300
-initialize_and_train_video_allmethods sear_steak 1 1000 0.3 8 300
+initialize_and_train_video_allmethods coffee_martini 1 1000 0.3 9 300
+initialize_and_train_video_allmethods cook_spinach 1 1000 0.3 9 300
+initialize_and_train_video_allmethods cut_roasted_beef 1 1000 0.3 9 300
+initialize_and_train_video_allmethods flame_salmon_1 1 1000 0.3 9 1200
+initialize_and_train_video_allmethods flame_steak 1 1000 0.3 9 300
+initialize_and_train_video_allmethods sear_steak 1 1000 0.3 9 300
 
-initialize_and_train_video_allmethods discussion 1 1000 0.5 8 300
-initialize_and_train_video_allmethods stepin 1 1000 0.5 8 300
-initialize_and_train_video_allmethods trimming 1 1000 0.5 8 300
-initialize_and_train_video_allmethods vrheadset 1 1000 0.5 8 300
+initialize_and_train_video_allmethods discussion 1 1000 0.5 9 300
+initialize_and_train_video_allmethods stepin 1 1000 0.5 9 300
+initialize_and_train_video_allmethods trimming 1 1000 0.5 9 300
+initialize_and_train_video_allmethods vrheadset 1 1000 0.5 9 300
 
-initialize_and_train_video_allmethods basketball 1 1000 1.0 8 150
-initialize_and_train_video_allmethods boxes 1 1000 1.0 8 150
-initialize_and_train_video_allmethods football 1 1000 1.0 8 150
-initialize_and_train_video_allmethods juggle 1 1000 1.0 8 150
-initialize_and_train_video_allmethods softball 1 1000 1.0 8 150
-initialize_and_train_video_allmethods tennis 1 1000 1.0 8 150
+initialize_and_train_video_allmethods basketball 1 1000 1.0 9 150
+initialize_and_train_video_allmethods boxes 1 1000 1.0 9 150
+initialize_and_train_video_allmethods football 1 1000 1.0 9 150
+initialize_and_train_video_allmethods juggle 1 1000 1.0 9 150
+initialize_and_train_video_allmethods softball 1 1000 1.0 9 150
+initialize_and_train_video_allmethods tennis 1 1000 1.0 9 150
