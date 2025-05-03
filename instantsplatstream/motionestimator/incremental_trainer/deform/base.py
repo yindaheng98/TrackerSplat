e98b393a245ef6b09d35d1b209a1be197766fc77
yindaheng98 <yindaheng98@gmail.com>
2025-05-03 16:18:14 -0700

    hexplane bug fixed

--- instantsplatstream/motionestimator/incremental_trainer/deform/base.py
+++ instantsplatstream/motionestimator/incremental_trainer/deform/base.py
@@ -1,7 +1,7 @@
-from typing import Callable, Dict
+import copy
 import torch
 from gaussian_splatting import GaussianModel, Camera
-from gaussian_splatting.trainer import AbstractTrainer
+from gaussian_splatting.trainer import BaseTrainer
 from gaussian_splatting.utils import l1_loss, ssim
 from gaussian_splatting.utils.schedular import get_expon_lr_func
 from instantsplatstream.motionestimator.fixedview import FixedViewFrameSequenceMetaDataset
@@ -10,52 +10,54 @@ from instantsplatstream.motionestimator.incremental_trainer import TrainerFactor
 from .deformation import DeformNetwork
 
 
-class HexplaneTrainer(AbstractTrainer):
+class HexplaneTrainer(BaseTrainer):
 
     def __init__(
             self, model: GaussianModel,
             basemodel: GaussianModel,
             spatial_lr_scale: float,
-            lambda_dssim=0.2,
-            feature_lr=0.0025,
-            position_lr_max_steps=30_000,
             # https://github.com/wanglids/ST-4DGS/blob/bf0dbb13e76bf41b2c2a4ca64063e5d346db7c74/arguments/__init__.py
+            lambda_dssim=0.2,
+            position_lr_init=0.00016,
+            position_lr_final=0.0000016,
+            position_lr_delay_mult=0.01,
+            position_lr_max_steps=20_000,
+            feature_lr=0.0025,
+            opacity_lr=0.05,
+            scaling_lr=0.005,
+            rotation_lr=0.001,
             deformation_lr_init=0.00016,
             deformation_lr_final=0.000016,
             deformation_lr_delay_mult=0.01,
             grid_lr_init=0.0016,
             grid_lr_final=0.00016,
             **kwargs):
-        self._model = GaussianModel(model.max_sh_degree)
-        self._model._features_dc = model._features_dc
-        self._model._features_rest = model._features_rest
-        self.lambda_dssim = lambda_dssim
-        params = [
-            {'params': [model._features_dc], 'lr': feature_lr, "name": "f_dc"},
-            {'params': [model._features_rest], 'lr': feature_lr / 20.0, "name": "f_rest"},
-        ]
-        self._deformation = DeformNetwork(**kwargs).to(model._xyz.device)
-
+        super().__init__(
+            model, spatial_lr_scale,
+            lambda_dssim=lambda_dssim,
+            position_lr_init=position_lr_init,
+            position_lr_final=position_lr_final,
+            position_lr_delay_mult=position_lr_delay_mult,
+            position_lr_max_steps=position_lr_max_steps,
+            feature_lr=feature_lr,
+            opacity_lr=opacity_lr,
+            scaling_lr=scaling_lr,
+            rotation_lr=rotation_lr
+        )
         # https://github.com/wanglids/ST-4DGS/blob/bf0dbb13e76bf41b2c2a4ca64063e5d346db7c74/scene/gaussian_model.py#L162
-        params += [
-            {'params': list(self._deformation.get_mlp_parameters()), 'lr': deformation_lr_init * spatial_lr_scale, "name": "deformation"},
-            {'params': list(self._deformation.get_grid_parameters()), 'lr': grid_lr_init * spatial_lr_scale, "name": "grid"},
-        ]
-        optimizer = torch.optim.Adam(params, lr=0.0, eps=1e-15)
-        schedulers = {
-            get_expon_lr_func(
-                lr_init=deformation_lr_init*spatial_lr_scale,
-                lr_final=deformation_lr_final*spatial_lr_scale,
-                lr_delay_mult=deformation_lr_delay_mult,
-                max_steps=position_lr_max_steps),
-            get_expon_lr_func(
-                lr_init=grid_lr_init*spatial_lr_scale,
-                lr_final=grid_lr_final*spatial_lr_scale,
-                lr_delay_mult=deformation_lr_delay_mult,
-                max_steps=position_lr_max_steps)
-        }
-        self._optimizer = optimizer
-        self._schedulers = schedulers
+        self._deformation = DeformNetwork(**kwargs).to(model._xyz.device)
+        self.optimizer.add_param_group({'params': list(self._deformation.get_mlp_parameters()), 'lr': deformation_lr_init * spatial_lr_scale, "name": "deformation"})
+        self.optimizer.add_param_group({'params': list(self._deformation.get_grid_parameters()), 'lr': grid_lr_init * spatial_lr_scale, "name": "grid"})
+        self.schedulers["deformation"] = get_expon_lr_func(
+            lr_init=deformation_lr_init*spatial_lr_scale,
+            lr_final=deformation_lr_final*spatial_lr_scale,
+            lr_delay_mult=deformation_lr_delay_mult,
+            max_steps=position_lr_max_steps)
+        self.schedulers["grid"] = get_expon_lr_func(
+            lr_init=grid_lr_init*spatial_lr_scale,
+            lr_final=grid_lr_final*spatial_lr_scale,
+            lr_delay_mult=deformation_lr_delay_mult,
+            max_steps=position_lr_max_steps)
         self._curr_step = 0
 
         self.mean3D_base = basemodel._xyz.detach()
@@ -68,29 +70,21 @@ class HexplaneTrainer(AbstractTrainer):
         self.per_time = torch.tensor(0).to(model._xyz.device).repeat(model._xyz.shape[0], 1)
 
     @property
-    def curr_step(self) -> int:
-        return self._curr_step
-
-    @curr_step.setter
-    def curr_step(self, v):
-        self._curr_step = v
-
-    @property
     def model(self) -> GaussianModel:
         means3D_deform, scales_deform, rotations_deform, opacity_deform = self._deformation(self.mean3D_base, self.scales_base, self.rotations_base, self.opacity_base, self.per_time)
+        if hasattr(self._model, "_xyz"):
+            del self._model._xyz
+        if hasattr(self._model, "_scaling"):
+            del self._model._scaling
+        if hasattr(self._model, "_rotation"):
+            del self._model._rotation
+        if hasattr(self._model, "_opacity"):
+            del self._model._opacity
         self._model._xyz = means3D_deform
         self._model._scaling = scales_deform
         self._model._rotation = rotations_deform
         self._model._opacity = opacity_deform
         return self._model
-
-    @property
-    def optimizer(self) -> torch.optim.Optimizer:
-        return self._optimizer
-
-    @property
-    def schedulers(self) -> Dict[str, Callable[[int], float]]:
-        return self._schedulers
 
     def loss(self, out: dict, camera: Camera) -> torch.Tensor:
         render = out["render"]
