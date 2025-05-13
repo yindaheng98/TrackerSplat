c075147f50e0e1018ccf3dfc5ddb1c330891c58b
yindaheng98 <yindaheng98@gmail.com>
2025-05-13 03:53:42 +0000

    update speedtest

--- tools/speedtest.sh
+++ tools/speedtest.sh
@@ -18,28 +18,27 @@ speedtest_allmethods() {
     speedtest $1 $2 $3 refine/base-propagate-dot-cotracker3 "-o rescale_factor=$4" $5
     speedtest $1 $2 $3 refine/base-base-dot-cotracker3 "-o rescale_factor=$4" $5
     speedtest $1 $2 $3 train/regularized "-o neighbors=20" $5
-    speedtest $1 $2 $3 train/base "" $5
+    speedtest $1 $2 $3 train/hicom "" $5
     speedtest $1 $2 $3 train/hexplane "" $5
     speedtest $1 $2 $3 train/regularizedhexplane "-o neighbors=20" $5
 }
 
 DEVICES=4
 
-FRAME=51
+FRAME=1
 speedtest_allmethods "walking" $FRAME 1000 0.3 $DEVICES
 speedtest_allmethods "taekwondo" $FRAME 1000 0.3 $DEVICES
+speedtest_allmethods "boxing" $FRAME 1000 0.3 $DEVICES
 
 speedtest_allmethods "coffee_martini" $FRAME 1000 0.3 $DEVICES
 speedtest_allmethods "cook_spinach" $FRAME 1000 0.3 $DEVICES
 speedtest_allmethods "cut_roasted_beef" $FRAME 1000 0.3 $DEVICES
 
-INITARGS="-o use_fused=True"
 speedtest_allmethods "discussion" $FRAME 1000 0.5 $DEVICES
 speedtest_allmethods "stepin" $FRAME 1000 0.5 $DEVICES
 speedtest_allmethods "trimming" $FRAME 1000 0.5 $DEVICES
 speedtest_allmethods "vrheadset" $FRAME 1000 0.5 $DEVICES
 
-INITARGS="-o use_fused=True"
 speedtest_allmethods "basketball" $FRAME 1000 1.0 $DEVICES
 speedtest_allmethods "boxes" $FRAME 1000 1.0 $DEVICES
 speedtest_allmethods "football" $FRAME 1000 1.0 $DEVICES
