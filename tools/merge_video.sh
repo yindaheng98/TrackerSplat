2da3534dcb51a91bcb8beb3e9871914201cb862a
yindaheng98 <yindaheng98@gmail.com>
2025-05-23 15:17:12 -0700

    merge the videos

---
+++ tools/merge_video.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+FFMPEG="ffmpeg"
+merge() {
+    python tools/merge_video.py output $1 $2 $3 output/collected_videos
+    $FFMPEG -y -f image2 -framerate 15 -i output/collected_videos/$1/%05d.png -vcodec libx264 -pix_fmt yuv420p -crf 20 -preset veryslow output/collected_videos/$1.mp4
+}
+merge walking st-nerf 75
+merge taekwondo st-nerf 101
+merge boxing st-nerf 71
+
+merge coffee_martini N3DV 300
+merge cook_spinach N3DV 300
+merge cut_roasted_beef N3DV 300
+merge flame_salmon_1 N3DV 1200
+merge flame_steak N3DV 300
+merge sear_steak N3DV 300
+
+merge discussion MeetingRoom 300
+merge stepin MeetingRoom 300
+merge trimming MeetingRoom 300
+merge vrheadset MeetingRoom 300
+
+merge basketball Dynamic3DGS 150
+merg boxese Dynamic3DGS 150
+merge football Dynamic3DGS 150
+merge juggle Dynamic3DGS 150
+merge softball Dynamic3DGS 150
+merge tennis Dynamic3DGS 150
