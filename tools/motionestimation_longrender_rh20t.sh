1e915ad471df411fba2c8111a078250ff36347db
yindaheng98 <yindaheng98@gmail.com>
2025-09-14 06:07:18 -0700

    render rh20t

---
+++ tools/motionestimation_longrender_rh20t.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+FFMPEG="ffmpeg"
+render() {
+    for i in $(seq $5 $6); do
+        EXISTSPATH="output/$1/$2/frame$i/point_cloud/iteration_$4/point_cloud.ply"
+        if [ ! -e "$EXISTSPATH" ]; then
+            echo "not exists: $EXISTSPATH"
+            return
+        fi
+    done
+    echo "all exists: output/$1/$2/frame<$5-$6>/point_cloud/iteration_$3/point_cloud.ply"
+    python -m instantsplatstream.render \
+        -d output/$1/$2 \
+        --data_dir output/$1/$2/render \
+        --destination_init output/$1/frame1 \
+        --iteration_init $3 -i $4 \
+        --frame_start $5 --frame_end $6 \
+        --load_camera output/$1/frame1/cameras.json \
+        --mode base
+    $FFMPEG -y -f image2 -i output/$1/$2/render/%05d.png -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -pix_fmt yuv420p -crf 10 output/$1/$2.mp4
+    rm -rf output/$1/$2/render
+}
+n_frames() {
+    n=0
+    while [ -d "data/$1/frame$(expr $n + 1)" ]; do
+        n=$(expr $n + 1)
+    done
+    echo $n
+}
+render_all() {
+    N=$(n_frames $1)
+    render $1 refine/base-propagate-dot-cotracker3 $2 $3 2 $N
+    render $1 refine/base-base-dot-cotracker3 $2 $3 2 $N
+    render $1 train/regularized $2 $3 2 $N
+    render $1 train/hicom $2 $3 2 $N
+    render $1 train/hexplane $2 $3 2 $N
+    render $1 train/regularizedhexplane $2 $3 2 $N
+}
+# render_all RH20T_cfg3/task_0001_user_0016_scene_0002_cfg_0003 10000 1000 # debug
+for s in data/RH20T_cfg3/task_*_user_*_scene_*_cfg_0003; do
+    render_all ${s:5} 10000 1000 # debug
+done
