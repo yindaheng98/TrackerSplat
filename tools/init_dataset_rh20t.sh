094385fd00e94530bd7bd19ec0bae58c2b22dca8
yindaheng98 <yindaheng98@gmail.com>
2025-09-14 02:07:03 -0700

    copy sparse to later frames

--- tools/init_dataset_rh20t.sh
+++ tools/init_dataset_rh20t.sh
@@ -6,6 +6,12 @@ initialize() {
     python -m instantsplat.initialize \
         -d data/$1/frame1 \
         --initializer dust3r
+    n=2
+    while [ -d "data/$1/frame$n" ]; do
+        rm -rf data/$1/frame$n/sparse
+        cp -r data/$1/frame1/sparse data/$1/frame$n
+        n=$(expr $n + 1)
+    done
 }
 
 initialize RH20T_cfg3/task_0001_user_0016_scene_0002_cfg_0003
