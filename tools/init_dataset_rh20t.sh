44ca988f9dda9f3d036c73455f517d3e15cad396
yindaheng98 <yindaheng98@gmail.com>
2025-09-14 18:08:22 -0700

    use RH20T_cfg5 for more camera

--- tools/init_dataset_rh20t.sh
+++ tools/init_dataset_rh20t.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+# rm -rf data/RH20T_cfg5 && tar -zxvf data/RH20T_cfg5.tar.gz -C data
 initialize() {
     rm -rf data/$1/frame*
-    python tools/extract_rh20t.py --path data/$1 --except 045322071843 --except 135122079769
+    python tools/extract_rh20t.py --path data/$1 --except 104422070042 --except 135122079702 --except 104422071090
     python -m instantsplat.initialize \
         -d data/$1/frame1 \
         --initializer dust3r
@@ -13,8 +13,8 @@ initialize() {
         n=$(expr $n + 1)
     done
 }
-# initialize RH20T_cfg3/task_0001_user_0016_scene_0002_cfg_0003 # debug
+# initialize RH20T_cfg5/task_0001_user_0016_scene_0002_cfg_0005 # debug
 
-for s in data/RH20T_cfg3/task_*_user_*_scene_*_cfg_0003; do
+for s in data/RH20T_cfg5/task_*_user_*_scene_*_cfg_0005; do
     initialize ${s:5}
 done
