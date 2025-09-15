d34451e4386c5baae4b4f77bd1f7413ca30e02ca
yindaheng98 <yindaheng98@gmail.com>
2025-09-14 20:08:09 -0700

    slow down render

--- tools/motionestimation_longrender_rh20t.sh
+++ tools/motionestimation_longrender_rh20t.sh
@@ -15,6 +15,7 @@ render() {
         --destination_init output/$1/frame1 \
         --iteration_init $3 -i $4 \
         --frame_start $5 --frame_end $6 \
+        --interp_n $7 \
         --load_camera output/$1/frame1/cameras.json \
         --mode base
     $FFMPEG -y -f image2 -i output/$1/$2/render/%05d.png -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -pix_fmt yuv420p -crf 10 output/$1/$2.mp4
@@ -29,14 +30,15 @@ n_frames() {
 }
 render_all() {
     N=$(n_frames $1)
-    render $1 refine/base-propagate-dot-cotracker3 $2 $3 2 $N
-    render $1 refine/base-base-dot-cotracker3 $2 $3 2 $N
-    render $1 train/regularized $2 $3 2 $N
-    render $1 train/hicom $2 $3 2 $N
-    render $1 train/hexplane $2 $3 2 $N
-    render $1 train/regularizedhexplane $2 $3 2 $N
+    NI=$(expr $N \* 10)
+    render $1 refine/base-propagate-dot-cotracker3 $2 $3 2 $N $NI
+    render $1 refine/base-base-dot-cotracker3 $2 $3 2 $N $NI
+    render $1 train/regularized $2 $3 2 $N $NI
+    render $1 train/hicom $2 $3 2 $N $NI
+    render $1 train/hexplane $2 $3 2 $N $NI
+    render $1 train/regularizedhexplane $2 $3 2 $N $NI
 }
 # render_all RH20T_cfg5/task_0001_user_0016_scene_0002_cfg_0005 10000 1000 # debug
-for s in data/RH20T_cfg5/task_*_user_*_scene_*_cfg_0005; do
-    render_all ${s:5} 10000 1000 # debug
+for s in output/RH20T_cfg5/task_*_user_*_scene_*_cfg_0005; do
+    render_all ${s:7} 10000 1000 # debug
 done
