7c3c8208c4ebf390e7026ee8addb5984fdf7e5de
yindaheng98 <yindaheng98@gmail.com>
2025-10-25 04:55:23 -0700

    extract and organize data

--- tools/init_SIGA2025VVC.sh
+++ tools/init_SIGA2025VVC.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 extract_SIGA2025VVC() {
-    mkdir -p data/SIGA2025VVC-Dataset/$1/images
-    tar -zxvf data/SIGA2025VVC-Dataset/$1/images.tar.gz -C data/SIGA2025VVC-Dataset/$1
-    mkdir -p data/SIGA2025VVC-Dataset/$1/masks
-    tar -zxvf data/SIGA2025VVC-Dataset/$1/masks.tar.gz -C data/SIGA2025VVC-Dataset/$1
+    ROOT=data/SIGA2025VVC-Dataset/$1
+    rm -rf $ROOT/images
+    tar -zxvf $ROOT/images.tar.gz -C $ROOT
+    rm -rf $ROOT/masks
+    tar -zxvf $ROOT/masks.tar.gz -C $ROOT
+    for frame in $(seq 0 $2); do
+        rm -rf $ROOT/frame$((frame + 1))
+        mkdir -p $ROOT/frame$((frame + 1))/images
+        for view in $(seq 0 $3); do
+            mv $ROOT/images/$(printf "%02d" $view)/$(printf "%06d" $frame).jpg $ROOT/frame$((frame + 1))/images/$(printf "%02d" $view).jpg
+            mv $ROOT/masks/$(printf "%02d" $view)/$(printf "%06d" $frame).png $ROOT/frame$((frame + 1))/images/$(printf "%02d" $view)_mask.png
+        done
+    done
 }
-extract_SIGA2025VVC compression/test/004_1_seq1 # debug
+extract_SIGA2025VVC compression/test/004_1_seq1 300 57 # debug
