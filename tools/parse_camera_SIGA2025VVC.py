132119fb93d85860290d18e7df1a40ff8bf7fea7
yindaheng98 <yindaheng98@gmail.com>
2025-10-26 03:00:21 -0700

    build_frame_folder for SIGA2025VVC

--- tools/parse_camera_SIGA2025VVC.py
+++ tools/parse_camera_SIGA2025VVC.py
@@ -5,8 +5,12 @@ import os
 from os.path import dirname, exists, join
 import argparse
 from argparse import Namespace
+from tqdm import tqdm
 import numpy as np
 import cv2
+from PIL import Image
+import torch
+import torch.nn.functional as F
 
 
 class DotDict(dict):
@@ -180,10 +184,132 @@ def read_camera(intri_path: str, extri_path: str = None, cam_names=[]) -> dotdic
     return dotdict(cams)
 
 
+def standardize_quaternion(quaternions: torch.Tensor) -> torch.Tensor:
+    """
+    Convert a unit quaternion to a standard form: one in which the real
+    part is non negative.
+
+    Args:
+        quaternions: Quaternions with real part first,
+            as tensor of shape (..., 4).
+
+    Returns:
+        Standardized quaternions as tensor of shape (..., 4).
+    Source: https://pytorch3d.readthedocs.io/en/latest/_modules/pytorch3d/transforms/rotation_conversions.html
+    """
+    return torch.where(quaternions[..., 0:1] < 0, -quaternions, quaternions)
+
+
+def _sqrt_positive_part(x: torch.Tensor) -> torch.Tensor:
+    """
+    Returns torch.sqrt(torch.max(0, x))
+    but with a zero subgradient where x is 0.
+    Source: https://pytorch3d.readthedocs.io/en/latest/_modules/pytorch3d/transforms/rotation_conversions.html
+    """
+    ret = torch.zeros_like(x)
+    positive_mask = x > 0
+    # if torch.is_grad_enabled():
+    #     ret[positive_mask] = torch.sqrt(x[positive_mask])
+    # else:
+    #     ret = torch.where(positive_mask, torch.sqrt(x), ret)
+    ret[positive_mask] = torch.sqrt(x[positive_mask])
+    return ret
+
+
+def matrix_to_quaternion(matrix: torch.Tensor) -> torch.Tensor:
+    """
+    Convert rotations given as rotation matrices to quaternions.
+
+    Args:
+        matrix: Rotation matrices as tensor of shape (..., 3, 3).
+
+    Returns:
+        quaternions with real part first, as tensor of shape (..., 4).
+    Source: https://pytorch3d.readthedocs.io/en/latest/_modules/pytorch3d/transforms/rotation_conversions.html
+    """
+    if matrix.size(-1) != 3 or matrix.size(-2) != 3:
+        raise ValueError(f"Invalid rotation matrix shape {matrix.shape}.")
+
+    batch_dim = matrix.shape[:-2]
+    m00, m01, m02, m10, m11, m12, m20, m21, m22 = torch.unbind(
+        matrix.reshape(batch_dim + (9,)), dim=-1
+    )
+
+    q_abs = _sqrt_positive_part(
+        torch.stack(
+            [
+                1.0 + m00 + m11 + m22,
+                1.0 + m00 - m11 - m22,
+                1.0 - m00 + m11 - m22,
+                1.0 - m00 - m11 + m22,
+            ],
+            dim=-1,
+        )
+    )
+
+    # we produce the desired quaternion multiplied by each of r, i, j, k
+    quat_by_rijk = torch.stack(
+        [
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([q_abs[..., 0] ** 2, m21 - m12, m02 - m20, m10 - m01], dim=-1),
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([m21 - m12, q_abs[..., 1] ** 2, m10 + m01, m02 + m20], dim=-1),
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([m02 - m20, m10 + m01, q_abs[..., 2] ** 2, m12 + m21], dim=-1),
+            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
+            #  `int`.
+            torch.stack([m10 - m01, m20 + m02, m21 + m12, q_abs[..., 3] ** 2], dim=-1),
+        ],
+        dim=-2,
+    )
+
+    # We floor here at 0.1 but the exact level is not important; if q_abs is small,
+    # the candidate won't be picked.
+    flr = torch.tensor(0.1).to(dtype=q_abs.dtype, device=q_abs.device)
+    quat_candidates = quat_by_rijk / (2.0 * q_abs[..., None].max(flr))
+
+    # if not for numerical problems, quat_candidates[i] should be same (up to a sign),
+    # forall i; we pick the best-conditioned one (with the largest denominator)
+    out = quat_candidates[
+        F.one_hot(q_abs.argmax(dim=-1), num_classes=4) > 0.5, :
+    ].reshape(batch_dim + (4,))
+    return standardize_quaternion(out)
+
+
+def build_frame_folder_SIGA2025VVC(camera_meta, folder, frame):
+    os.makedirs(os.path.join(folder, "images"), exist_ok=True)
+    cameras, images = {}, {}
+    for i, meta in camera_meta.items():
+        img_name = f"{i}.jpg"
+
+        fx, fy = meta['K'][0][0], meta['K'][1][1]
+        cx, cy = meta['K'][0][2], meta['K'][1][2]
+        width, height = int(cx * 2), int(cy * 2)
+        cameras[img_name] = f"PINHOLE {width} {height} {fx} {fy} {cx} {cy}"
+
+        R, T = torch.tensor(meta['R']), torch.tensor(meta['T']).squeeze()
+        q, t = matrix_to_quaternion(R), T
+        images[img_name] = f"{q[0]} {q[1]} {q[2]} {q[3]} {t[0]} {t[1]} {t[2]}"
+
+        img_src = os.path.join(args.path, "images", i, f"{frame:06d}.jpg")
+        img_dst = os.path.join(folder, "images", img_name)
+        if os.path.isfile(img_dst):
+            os.remove(img_dst)
+        os.link(img_src, img_dst)
+    return cameras, images
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", type=str, required=True, help="path to the video folder")
+parser.add_argument("--n_frames", type=int, default=300, help="number of frames")
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    cameras = read_camera_new(args.path, intri_name="train_intri.yml", extri_name="train_extri.yml")
-    print(cameras)
+    camera_meta = read_camera_new(args.path, intri_name="train_intri.yml", extri_name="train_extri.yml")
+    for frame in tqdm(range(args.n_frames), desc="Linking frames"):
+        folder = os.path.join(args.path, "frame%d" % (frame + 1))
+        cameras, images = build_frame_folder_SIGA2025VVC(camera_meta, folder, frame)
+        print(cameras, images)
