f6662ffcb19ead9af9807b9cb60662a52526d662
yindaheng98 <yindaheng98@gmail.com>
2025-11-13 20:16:22 -0800

    better args

--- tools/motionestimation_long_SIGA2025VVC.sh
+++ tools/motionestimation_long_SIGA2025VVC.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 INITTRAININGITERS=10000
-# initialize "walking" 1 # debug
-REFININGARGS=""
-REFININGARGS=$REFININGARGS" -rscaling_lr=0.000001"
 train() {
     ok=true
     for i in $(seq $(expr $2 + 1) $(expr $2 + $7 - 1)); do
@@ -23,22 +20,40 @@ train() {
     # echo \
     python -m trackersplat.motionestimation \
         -s data/$1 -d output/$1 --start_frame $2 \
-        --iteration_init $INITTRAININGITERS -i $3 -rposition_lr_max_steps=$3 \
+        --iteration_init $INITTRAININGITERS -i $3 \
         --pipeline $4 $5 \
-        -b $6 -n $7 $REFININGARGS \
+        -b $6 -n $7 \
         --load_camera $8 \
-        --with_image_mask "-rmask_mode='bg_color'" -omask_input=False -omask_output=False
+        --with_image_mask
 }
-# train "walking" 1 1000 track/propagate-dot-cotracker3 "" 9 100 "output/walking/frame1/cameras.json" # debug
+
+REFINEARGS="$REFINEARGS -omask_input=False"
+REFINEARGS="$REFINEARGS -omask_output=False"
+
+REFINEARGS="$REFINEARGS -rposition_lr_init=0.0016"
+REFINEARGS="$REFINEARGS -rposition_lr_max_steps=1000"
+REFINEARGS="$REFINEARGS -rscaling_lr=0.000001"
+REFINEARGS="$REFINEARGS -ropacity_lr=0.000001"
+REFINEARGS="$REFINEARGS -rmask_mode='bg_color'"
+REFINEARGS="$REFINEARGS -rbg_color='random'"
+
+TRAINARGS="$TRAINARGS -oposition_lr_init=0.0016"
+TRAINARGS="$TRAINARGS -oposition_lr_max_steps=1000"
+TRAINARGS="$TRAINARGS -oscaling_lr=0.000001"
+TRAINARGS="$TRAINARGS -oopacity_lr=0.000001"
+TRAINARGS="$TRAINARGS -omask_mode='bg_color'"
+TRAINARGS="$TRAINARGS -orbg_color='random'"
+
 initialize_and_train_video_allmethods() {
     CAMERAS="output/$1/frame$2/cameras.json"
-    train $1 $2 $3 refine/base-propagate-dot-cotracker3 "-o rescale_factor=$4" $5 $6 "$CAMERAS"
+    train $1 $2 $3 refine/base-propagate-dot-cotracker3 "-o rescale_factor=$4 $REFINEARGS" $5 $6 "$CAMERAS"
+    train $1 $2 $3 train/regularized "-o neighbors=8 -o loss_weight_overall=0.1 $TRAINARGS" $5 $6 "$CAMERAS"
 }
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/004_1_seq1 1 1000 0.2 9 300
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/006_1_seq1 1 1000 0.2 9 300
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/007_0_seq1 1 1000 0.2 9 300
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/008_2_seq1 1 1000 0.2 9 300
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/008_2_seq1 1 1000 0.2 9 300
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/011_0_seq1 1 1000 0.2 9 300
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/val/001_1_seq0 1 1000 0.2 9 300
-initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/val/012_0_seq0 1 1000 0.2 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/004_1_seq1 1 1000 0.25 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/006_1_seq1 1 1000 0.25 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/007_0_seq1 1 1000 0.25 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/008_2_seq1 1 1000 0.25 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/008_2_seq1 1 1000 0.25 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/test/011_0_seq1 1 1000 0.25 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/val/001_1_seq0 1 1000 0.25 9 300
+initialize_and_train_video_allmethods SIGA2025VVC-Dataset/compression/val/012_0_seq0 1 1000 0.25 9 300
