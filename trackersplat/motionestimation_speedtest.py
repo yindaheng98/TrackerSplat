5aad58178023dc98f01ce410c0b7f81d33204ef8
yindaheng98 <yindaheng98@gmail.com>
2025-11-14 11:16:01 -0800

    upgrade speed test

--- trackersplat/motionestimation_speedtest.py
+++ trackersplat/motionestimation_speedtest.py
@@ -1,4 +1,3 @@
-import itertools
 from typing import List
 import torch
 import torch.multiprocessing as mp
@@ -11,10 +10,11 @@ from gaussian_splatting.dataset import CameraDataset
 from gaussian_splatting.trainer import AbstractTrainer
 from trackersplat.dataset import prepare_fixedview_dataset, VideoCameraDataset
 from trackersplat.motionestimator import FixedViewMotionEstimator, MotionCompensater
-from trackersplat.motionestimator.point_tracker import BaseMotionFuser, build_point_track_batch_motion_estimator, DataParallelPointTrackMotionEstimator
+from trackersplat.motionestimator.point_tracker import DetectFixMotionFuser, build_point_track_batch_motion_estimator, DataParallelPointTrackMotionEstimator
 from trackersplat.motionestimator.compensater import BaseMotionCompensater, build_motion_compensater
-from trackersplat.motionestimator.incremental_trainer import IncrementalTrainingMotionEstimator, IncrementalTrainingRefiner, build_trainer_factory, TrainingProcess, BaseTrainingProcess
-from trackersplat.motionestimation import save_cfg_args, prepare_gaussians
+from trackersplat.motionestimator.incremental_trainer import IncrementalTrainingMotionEstimator, build_trainer_factory, BaseTrainingProcess
+from trackersplat.motionestimator.refiner import build_compensater_with_refine
+from trackersplat.motionestimation import save_cfg_args, prepare_gaussians, pipeline_choices
 
 
 class TimingDataParallelPointTrackMotionEstimator(DataParallelPointTrackMotionEstimator):
@@ -49,26 +49,11 @@ class TimingTrainingProcess(BaseTrainingProcess):
             f.write(f"training timing: {start.elapsed_time(end)}\n")
 
 
-estimator_choices = ["dot", "dot-tapir", "dot-bootstapir", "dot-cotracker3", "cotracker3"]
-compensater_choices = ["base", "propagate", "filter"]
-trainer_choices = ["base", "regularized", "masked", "maskregularized", "hexplane", "regularizedhexplane", "hicom"]
-
-train_choices = ["train/" + trainer for trainer in trainer_choices]
-refine_choices = ["refine/" + trainer + "-" + compensater + "-" + estimator for trainer, compensater, estimator in itertools.product(trainer_choices, compensater_choices, estimator_choices)]
-track_choices = ["track/" + compensater + "-" + estimator for compensater, estimator in itertools.product(compensater_choices, estimator_choices)]
-
-pipeline_choices = train_choices + refine_choices + track_choices
-
-
 def point_track_builder(estimator: str, gaussians: GaussianModel, **kwargs):
-    return build_point_track_batch_motion_estimator(estimator=estimator, fuser=BaseMotionFuser(gaussians.to("cuda")), device="cuda", **kwargs)
+    return build_point_track_batch_motion_estimator(estimator=estimator, fuser=DetectFixMotionFuser(gaussians.to("cuda")), device="cuda", **kwargs)
 
 
-def incremental_trainer_builder(builder, trainer: str, iteration: int, **kwargs):
-    return builder(trainer_factory=build_trainer_factory(trainer, **kwargs), iteration=iteration, device="cuda")
-
-
-def build_pipeline(pipeline: str, gaussians: GaussianModel, dataset: VideoCameraDataset, log_path: str, device: torch.device, device_ids: List[torch.device], batch_size: int, iteration: int, **kwargs) -> MotionCompensater:
+def build_pipeline(pipeline: str, gaussians: GaussianModel, dataset: VideoCameraDataset, log_path: str, device: torch.device, device_ids: List[torch.device], batch_size: int, iteration: int, configs_refining: dict, **kwargs) -> MotionCompensater:
     training_proc = TimingTrainingProcess()
     training_proc.log_path = log_path
     batch_func_return = None
@@ -98,9 +83,11 @@ def build_pipeline(pipeline: str, gaussians: GaussianModel, dataset: VideoCamera
         batch_func_return = batch_func
         motion_estimator = FixedViewMotionEstimator(dataset=dataset, batch_func=batch_func, device=device, batch_size=batch_size)
         motion_compensater = build_motion_compensater(compensater=compensater, gaussians=gaussians, estimator=motion_estimator, device=device)
-        batch_func = IncrementalTrainingRefiner(base_batch_func=batch_func, base_compensater=motion_compensater, trainer_factory=build_trainer_factory(trainer), training_proc=training_proc, iteration=iteration, device=device)
-        motion_estimator = FixedViewMotionEstimator(dataset=dataset, batch_func=batch_func, device=device, batch_size=batch_size)
-        motion_compensater = BaseMotionCompensater(gaussians=gaussians, estimator=motion_estimator, device=device)
+        motion_compensater = build_compensater_with_refine(
+            type="training", trainer=trainer, gaussians=gaussians, dataset=dataset, batch_size=batch_size,
+            base_batch_func=batch_func, base_compensater=motion_compensater, device=device,
+            training_proc=training_proc,
+            **configs_refining)
     else:
         ValueError(f"Unknown estimator: {estimator}")
     return batch_func_return, motion_compensater
@@ -131,33 +118,39 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--destination", required=True, type=str)
     parser.add_argument("-i", "--iteration", required=True, type=int)
     parser.add_argument("--load_camera", default=None, type=str)
+    parser.add_argument("--with_image_mask", action="store_true")
+    parser.add_argument("--with_depth_data", action="store_true")
     parser.add_argument("--device", default="cuda", type=str)
 
     parser.add_argument("--pipeline", choices=pipeline_choices, default="track/base-dot-cotracker3")
-    parser.add_argument("--parallel_device", required=True, action='append', type=int)
     parser.add_argument("--iteration_init", required=True, type=str, help="iteration of the initial gaussians")
     parser.add_argument("-f", "--frame_folder_fmt", default="frame%d", type=str, help="frame folder format string")
     parser.add_argument("-n", "--n_frames", default=None, type=int, help="number of frames to process")
     parser.add_argument("-b", "--batch_size", default=3, type=int, help="batch size of point tracking")
     parser.add_argument("--start_frame", default=1, type=int, help="start from which frame")
-    parser.add_argument("-o", "--option", default=[], action='append', type=str)
+    parser.add_argument("-o", "--option_estimation", default=[], action='append', type=str)
+    parser.add_argument("-r", "--option_refining", default=[], action='append', type=str)
+
+    parser.add_argument("--parallel_device", required=True, action='append', type=int)
     parser.add_argument("--save_frames", action='store_true')
     args = parser.parse_args()
     save_frame_cfg_args = partial(save_cfg_args, sh_degree=args.sh_degree, source=args.source, destination=os.path.join(args.destination, args.pipeline), frame_folder_fmt=args.frame_folder_fmt)
-    configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option}
+    configs = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option_estimation}
+    configs_refining = {o.split("=", 1)[0]: eval(o.split("=", 1)[1]) for o in args.option_refining}
     load_ply = os.path.join(args.destination, args.frame_folder_fmt % args.start_frame, "point_cloud", "iteration_" + str(args.iteration_init), "point_cloud.ply")
     gaussians = prepare_gaussians(
         sh_degree=args.sh_degree, device=args.device,
         load_ply=load_ply)
     dataset = prepare_fixedview_dataset(
         source=args.source, device=args.device,
-        frame_folder_fmt=args.frame_folder_fmt, start_frame=args.start_frame, n_frames=None,
-        load_camera=args.load_camera)
+        frame_folder_fmt=args.frame_folder_fmt, start_frame=args.start_frame, n_frames=args.n_frames,
+        load_camera=args.load_camera,
+        load_mask=args.with_image_mask, load_depth=args.with_depth_data)
 
     frame_str = (args.frame_folder_fmt % args.start_frame) + f"plus{args.batch_size - 1}to{args.n_frames + 1}"
     log_path = os.path.join(os.path.join(args.destination, args.pipeline), "timelog", frame_str + ".txt")
     os.makedirs(os.path.dirname(log_path), exist_ok=True)
     with open(log_path, "w") as f:
         pass
-    batch_func, motion_compensater = build_pipeline(args.pipeline, gaussians, dataset, log_path, args.device, args.parallel_device, args.batch_size, args.iteration, **configs)
+    batch_func, motion_compensater = build_pipeline(args.pipeline, gaussians, dataset, log_path, args.device, args.parallel_device, args.batch_size, args.iteration, configs_refining, **configs)
     motion_compensate(batch_func, motion_compensater, dataset, save_frame_cfg_args, args.iteration, args.start_frame, args.n_frames, args.save_frames)
