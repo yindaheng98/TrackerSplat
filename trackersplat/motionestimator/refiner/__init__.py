c77e79fa7ced9ea338932c620680a8be0e6e7e0d
yindaheng98 <yindaheng98@gmail.com>
2025-11-15 16:35:35 -0800

    name fix

--- trackersplat/motionestimator/refiner/__init__.py
+++ trackersplat/motionestimator/refiner/__init__.py
@@ -2,7 +2,7 @@
 This module implements refinement based on incremental training.
 Core abstract class: IncrementalTrainingRefiner
 """
-from .build import build_compensater_with_refine
+from .build import build_motion_estimator_with_refine
 from .training import build_training_refiner, IncrementalTrainingRefiner
 from .filter import FilteredMotionRefiner
 from .propogate import PropagatedMotionRefiner
