d7755b4245b9390681f2d1511c8f2ba73b910b65
yindaheng98 <yindaheng98@gmail.com>
2025-11-15 20:37:28 -0800

    dataset to device

--- trackersplat/patcher/abc.py
+++ trackersplat/patcher/abc.py
@@ -1,13 +1,19 @@
 from abc import ABCMeta, abstractmethod
 from gaussian_splatting import GaussianModel
+import torch
 from trackersplat import MotionCompensater
+from trackersplat import MotionEstimator
 from trackersplat.dataset import VideoCameraDataset, FrameCameraDataset
 
 
 class PatchCompensater(MotionCompensater, metaclass=ABCMeta):
-    def __init__(self, dataset: VideoCameraDataset, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, dataset: VideoCameraDataset, gaussians: GaussianModel, estimator: MotionEstimator, device: torch.device = "cuda"):  # 4 basic args
+        super().__init__(gaussians=gaussians, estimator=estimator, device=device)  # 3 basic args for MotionCompensater
         self.dataset = dataset
+
+    def to(self, device):
+        self.dataset = self.dataset.to(device)
+        return super().to(device)
 
     def __iter__(self) -> 'MotionCompensater':
         self.estimator = self.estimator.__iter__()
