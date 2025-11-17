3a07368e9adbe17ba8a6fc2e9e10e66a39966fc6
yindaheng98 <yindaheng98@gmail.com>
2025-11-16 18:23:24 -0800

    implemented GradientAttractDensifier

--- trackersplat/patcher/trainer.py
+++ trackersplat/patcher/trainer.py
@@ -8,28 +8,44 @@ from gaussian_splatting.trainer.densifier import AbstractDensifier, AdaptiveSpli
 class GradientAttractDensifier(AdaptiveSplitCloneDensifier):
     """Move low-opacity gaussians to high gradient areas."""
 
+    def __init__(
+        self,
+        *args,
+        densify_target_n=None,
+        **kwargs
+    ):
+        super().__init__(*args, densify_target_n=densify_target_n, **kwargs)
+
     def densify(self) -> DensificationInstruct:
         grads = self.xyz_gradient_accum / self.denom
         grads[grads.isnan()] = 0.0
 
-        gradscore = torch.norm(grads, dim=-1)
-        score_scaling = torch.max(self.model.get_scaling, dim=1).values
-        score_opacity = self.model.get_opacity.squeeze(-1)
-        score = score_scaling * score_opacity
-
         too_big_pts_mask = torch.max(self.model.get_scaling, dim=1).values > self.densify_percent_too_big*self.scene_extent
-        n_should_select = max(0, self.densify_target_n - grads.shape[0] - too_big_pts_mask.sum().item())
+        n_should_select = max(0, self.densify_target_n - too_big_pts_mask.sum().item()) if self.densify_target_n is not None else 0
         gradscore = torch.norm(grads, dim=-1)
-        gradscore_rest = gradscore[~too_big_pts_mask]
-        _, indices = torch.sort(gradscore_rest, descending=True)
-        grad_threshold = gradscore_rest[indices[min(n_should_select, gradscore_rest.shape[0]) - 1]].item()
         if n_should_select <= 0:
             grad_threshold = self.densify_grad_threshold
-        big_grad_pts_mask = gradscore >= min(grad_threshold, self.densify_grad_threshold)
+        else:
+            gradscore_rest = gradscore[~too_big_pts_mask]
+            _, indices = torch.sort(gradscore_rest, descending=True)
+            grad_threshold = gradscore_rest[indices[min(n_should_select, gradscore_rest.shape[0]) - 1]].item()
+        big_grad_pts_mask = gradscore >= max(grad_threshold, self.densify_grad_threshold)
         pts_mask = torch.logical_or(too_big_pts_mask, big_grad_pts_mask)
 
         clone = self.densify_and_clone(pts_mask, self.scene_extent)
         split = self.densify_and_split(pts_mask, self.scene_extent)
+
+        score_scaling = torch.max(self.model.get_scaling, dim=1).values
+        score_opacity = self.model.get_opacity.squeeze(-1)
+        score = score_scaling * score_opacity * (gradscore + gradscore[gradscore > 0].min())  # avoid zero
+
+        # delete pts_mask.sum().item() lowest score gaussians outside the selected ones
+        n_remove = min(pts_mask.sum().item(), score.shape[0] - pts_mask.sum().item())
+        rest_score = score[~pts_mask]
+        _, rest_indices = torch.sort(rest_score, descending=False)
+        remove_indices = rest_indices[:n_remove]
+        remove_mask = torch.zeros_like(pts_mask, dtype=torch.bool)
+        remove_mask[torch.arange(score.shape[0], device=score.device)[~pts_mask][remove_indices]] = True
 
         return DensificationInstruct(
             new_xyz=torch.cat((clone.new_xyz, split.new_xyz), dim=0),
@@ -38,7 +54,7 @@ class GradientAttractDensifier(AdaptiveSplitCloneDensifier):
             new_opacities=torch.cat((clone.new_opacities, split.new_opacities), dim=0),
             new_scaling=torch.cat((clone.new_scaling, split.new_scaling), dim=0),
             new_rotation=torch.cat((clone.new_rotation, split.new_rotation), dim=0),
-            remove_mask=split.remove_mask
+            remove_mask=split.remove_mask | remove_mask,
         )
 
 
@@ -53,6 +69,7 @@ def GradientAttractTrainerWrapper(
         densify_grad_threshold=0.0002,
         densify_percent_dense=0.01,
         densify_percent_too_big=0.8,
+        densify_target_n=None,  # ! this is different from usual AdaptiveSplitCloneDensifier, densify_target_n here is the target number of new gaussians to add at each densification step
         **kwargs):
     densifier = noargs_base_densifier_constructor(model, scene_extent)
     densifier = GradientAttractDensifier(
@@ -63,7 +80,8 @@ def GradientAttractTrainerWrapper(
         densify_interval=densify_interval,
         densify_grad_threshold=densify_grad_threshold,
         densify_percent_dense=densify_percent_dense,
-        densify_percent_too_big=densify_percent_too_big
+        densify_percent_too_big=densify_percent_too_big,
+        densify_target_n=densify_target_n,
     )
     return DensificationTrainer(
         model, scene_extent,
