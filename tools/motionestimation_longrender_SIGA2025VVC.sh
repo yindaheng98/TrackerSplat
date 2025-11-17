b963c98e1f6ec94b4138bb6114ffc31b39815651
yindaheng98 <yindaheng98@gmail.com>
2025-11-16 16:29:58 -0800

    render at 4000x3000

--- tools/motionestimation_longrender_SIGA2025VVC.sh
+++ tools/motionestimation_longrender_SIGA2025VVC.sh
@@ -16,12 +16,14 @@ render() {
         --iteration_init $3 -i $4 \
         --frame_start $5 --frame_end $6 \
         --load_camera output/$1/frame1/cameras.json \
-        --mode base
+        --mode base \
+        --use_intrinsics "dict(image_width=4000,FoVx=1.4749,image_height=3000,FoVy=1.1990)"
     $FFMPEG -y -f image2 -i output/$1/$2/render/%05d.png -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -pix_fmt yuv420p -crf 10 output/$1/$2.mp4
-    rm -rf output/$1/$2/render
+    # rm -rf output/$1/$2/render
 }
 render_all() {
     render $1 refine/base-propagate-dot-cotracker3 $2 $3 $4 $5
+    render $1 train/regularized $2 $3 $4 $5
 }
 render_all SIGA2025VVC-Dataset/compression/test/004_1_seq1 10000 1000 2 300
 render_all SIGA2025VVC-Dataset/compression/test/006_1_seq1 10000 1000 2 300
