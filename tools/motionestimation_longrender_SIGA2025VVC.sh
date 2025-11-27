35f86238a4c1a3f06c099015f9ab03912d0bf794
yindaheng98 <yindaheng98@gmail.com>
2025-11-26 23:07:47 -0800

    less

--- tools/motionestimation_longrender_SIGA2025VVC.sh
+++ tools/motionestimation_longrender_SIGA2025VVC.sh
@@ -29,7 +29,6 @@ render_all SIGA2025VVC-Dataset/compression/test/004_1_seq1 10000 1000 2 300
 render_all SIGA2025VVC-Dataset/compression/test/006_1_seq1 10000 1000 2 300
 render_all SIGA2025VVC-Dataset/compression/test/007_0_seq1 10000 1000 2 300
 render_all SIGA2025VVC-Dataset/compression/test/008_2_seq1 10000 1000 2 300
-render_all SIGA2025VVC-Dataset/compression/test/008_2_seq1 10000 1000 2 300
 render_all SIGA2025VVC-Dataset/compression/test/011_0_seq1 10000 1000 2 300
 render_all SIGA2025VVC-Dataset/compression/val/001_1_seq0 10000 1000 2 300
 render_all SIGA2025VVC-Dataset/compression/val/012_0_seq0 10000 1000 2 300
